5dfb84f18f59449afa8c72969a5aa7eeb2bd00e8
RonenZeyan <101258047+RonenZeyan@users.noreply.github.com>
2024-06-06 19:16:28 +0300

    add permission for our frontEnd from backend

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+
+// Import required modules
 const express = require("express") //use for node js 
 const mongoose = require("mongoose") //used mongoose for mongodb
 const cors = require("cors")
@@ -11,14 +13,19 @@ const bcrypt = require('bcrypt');  //we was want to use for hash the passwords b
 const jwt = require('jsonwebtoken');  //used for token
 const multer = require('multer');  //used for save uploaded pictures
 
+
+// Load environment variables like db url and port 
 dotenv.config()
 const port = process.env.PORT
 const url = process.env.DATABASE_URL
 
+// Initialize Express app
 const app = express()
-app.use(express.json())
+app.use(express.json()) // Use middleware to parse JSON requests
+
+// Configure CORS to allow access from the frontend
 app.use(cors({   //give the access from frontEnd
-    origin: ['http://localhost:3000', 'https://final-project-a19-frontend.vercel.app'],
+    origin: ['http://localhost:3000', 'https://a19-web-final-project-team-management.vercel.app/'],
     methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
     allowedHeaders: 'Content-Type,Authorization'
@@ -31,14 +38,17 @@ mongoose.connect(url)
 
 
 
-
-
+// Import routes
 const allUserRouter = require('./routes/AllUserRoute')
 const adminRouter = require('./routes/AdminRoute') 
 
+
+// Use routes
 app.use('/', allUserRouter )
 app.use('/', adminRouter )
 
+
+// Start the server
 app.listen(port,()=>{
     console.log("server is running")
 })
