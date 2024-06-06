f9747ba05eb26e1a75144f716f1881616888e73b
RonenZeyan <101258047+RonenZeyan@users.noreply.github.com>
2024-06-06 22:32:00 +0300

    fix calculate commit

--- client/src/constants.jsx
+++ client/src/constants.jsx
@@ -1,6 +1,6 @@
 
 
-const url = "http://localhost:3001"
-// const url = "https://a19-web-final-project-api.vercel.app"
+// const url = "http://localhost:3001"
+const url = "https://a19-web-final-project-api.vercel.app"
 
 export const API_URL = url
--- server/index.js
+++ server/index.js
@@ -1,11 +1,68 @@
 
+// // Import required modules
+// const express = require("express") //use for node js 
+// const mongoose = require("mongoose") //used mongoose for mongodb
+// const cors = require("cors")
+// //the models of db 
+// const UserModel = require('./models/Users')
+// // const dotenv = require('dotenv')
+
+// const MissionModel = require('./models/Missions')
+// const MsgModel = require('./models/MSG')
+// const bcrypt = require('bcrypt');  //we was want to use for hash the passwords but i dont know why its not working in my computer 
+// const jwt = require('jsonwebtoken');  //used for token
+// const multer = require('multer');  //used for save uploaded pictures
+
+
+// // Load environment variables like db url and port 
+// // dotenv.config()
+// // const port = process.env.PORT
+// // const url = process.env.DATABASE_URL
+
+// // Initialize Express app
+// const app = express()
+
+// app.use(express.json()) // Use middleware to parse JSON requests
+
+// app.use(cors({   //give the access from frontEnd
+//     origin: ['http://localhost:3000', 'https://a19-web-final-project-team-management.vercel.app/'],
+//     methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+//     credentials: true,
+//     allowedHeaders: ['Content-Type','Authorization']
+// }));
+
+
+
+// app.use(express.static('public'));  
+// // mongoose.connect("mongodb://localhost:27017/UsersMissionsManagement")
+// //connect to mongodb in cloud 
+// mongoose.connect("mongodb+srv://hunterman481:EWsTL72vV9XJOMFy@usersmissionsmanagement.byntanj.mongodb.net/?retryWrites=true&w=majority&appName=UsersMissionsManagement")
+
+
+
+// // Import routes
+// const allUserRouter = require('./routes/AllUserRoute')
+// const adminRouter = require('./routes/AdminRoute') 
+
+
+// // Use routes
+// app.use('/', allUserRouter )
+// app.use('/', adminRouter )
+
+
+// // Start the server
+// app.listen(3001,()=>{
+//     console.log("server is running")
+// })
+
+
 // Import required modules
 const express = require("express") //use for node js 
 const mongoose = require("mongoose") //used mongoose for mongodb
 const cors = require("cors")
 //the models of db 
 const UserModel = require('./models/Users')
-const dotenv = require('dotenv')
+// const dotenv = require('dotenv')
 
 const MissionModel = require('./models/Missions')
 const MsgModel = require('./models/MSG')
@@ -13,32 +70,20 @@ const bcrypt = require('bcrypt');  //we was want to use for hash the passwords b
 const jwt = require('jsonwebtoken');  //used for token
 const multer = require('multer');  //used for save uploaded pictures
 
-
-// Load environment variables like db url and port 
-dotenv.config()
-const port = process.env.PORT
-const url = process.env.DATABASE_URL
-
-// Initialize Express app
 const app = express()
-
-app.use(express.json()) // Use middleware to parse JSON requests
-
+app.use(express.json())
 app.use(cors({   //give the access from frontEnd
-    origin: ['http://localhost:3000', 'https://a19-web-final-project-team-management.vercel.app/'],
+    origin: 'https://a19-web-final-project-team-management.vercel.app',
+    // origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
-    allowedHeaders: ['Content-Type','Authorization']
+    allowedHeaders: 'Content-Type,Authorization'
 }));
-
-
 
 app.use(express.static('public'));  
 // mongoose.connect("mongodb://localhost:27017/UsersMissionsManagement")
 //connect to mongodb in cloud 
 mongoose.connect("mongodb+srv://hunterman481:EWsTL72vV9XJOMFy@usersmissionsmanagement.byntanj.mongodb.net/?retryWrites=true&w=majority&appName=UsersMissionsManagement")
-
-
 
 // Import routes
 const allUserRouter = require('./routes/AllUserRoute')
@@ -49,8 +94,6 @@ const adminRouter = require('./routes/AdminRoute')
 app.use('/', allUserRouter )
 app.use('/', adminRouter )
 
-
-// Start the server
 app.listen(3001,()=>{
     console.log("server is running")
 })
